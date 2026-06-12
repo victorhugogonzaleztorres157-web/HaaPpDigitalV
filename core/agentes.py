8722f8a1f96308bfa8a43a851e63d5581664c73b
victorhugogonzaleztorres157-web <victorhugogonzaleztorres157@gmail.com>
2026-06-12 02:04:14 -0600

    Delete core directory

--- core/agentes.py
+++
@@ -1,7 +0,0 @@
-class RedAgentes:
-    def __init__(self, cortex, osiris):
-        self.agentes = {}
-    
-    def registrar_agente(self, nombre, especialidad):
-        self.agentes[nombre] = especialidad
-        return f"Agente {nombre} listo."
